30fe511f3d57eab490ab86ece3928af94445ea7c
Matthew Meyer <mattchainsaw@gmail.com>
2015-03-29 21:35:26 -0500

    generalized database manager and added inventory class

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 
 public class DatabaseManager {
 
-
-
     private final static String DatabaseDriver = "org.sqlite.JDBC";
     private final static String SalonDatabase  = "jdbc:sqlite:Salon.db";
     private final static String EmployeeTable  = "EMPLOYEE";
@@ -55,6 +53,7 @@ public class DatabaseManager {
         }
     }
 
+    //// EMPLOYEE ////
     /**
      * Find all the employees in the database
      * @return ArrayList of all employees
@@ -72,50 +71,7 @@ public class DatabaseManager {
         return LookupEmployee(Integer.toString(attribute), field);
     }
     public static ArrayList<Employee> LookupEmployee(String attribute, String field) {
-        // check for empty args
-        if (attribute.equals("") && !field.equals("")) return null;
-        if (!attribute.equals("") && field.equals("")) return null;
-        // if both are empty, then select everything
-        String selector = (field.equals("") && attribute.equals("")) ?
-                "" : " WHERE " + field + "='" + attribute + "'";
-        ArrayList<Employee> employees = new ArrayList<Employee>();
-        Connection c = null;
-        Statement stmt = null;
-        try {
-            Class.forName(DatabaseDriver);
-            c = DriverManager.getConnection(SalonDatabase);
-            c.setAutoCommit(false);
-
-            try {
-                stmt = c.createStatement();
-                String sql = "SELECT * FROM " + EmployeeTable + selector;
-                ResultSet rs = stmt.executeQuery(sql);
-                while (rs.next()) {
-                    employees.add(new Employee(
-                            rs.getInt("id"),
-                            rs.getString("first_name"),
-                            rs.getString("last_name"),
-                            rs.getString("address"),
-                            rs.getString("city"),
-                            rs.getString("state"),
-                            rs.getString("phone"),
-                            rs.getString("email"),
-                            rs.getInt("salary"),
-                            ((rs.getString("manager")).equals("true"))));
-
-                }
-                rs.close();
-                stmt.close();
-            } catch (SQLException E) {
-                E.printStackTrace();
-            }
-            c.close();
-        } catch (SQLException E) {
-            E.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return employees;
+        return Get(attribute, field, EmployeeTable, new Employee());
     }
 
     /**
@@ -123,7 +79,51 @@ public class DatabaseManager {
      * @param worker employee to be added
      */
     public static void AddEmployee(Employee worker) {
-        String values = worker.toString();
+        Add(worker.toString(), EmployeeTable);
+    }
+
+    /**
+     * Makes the employee die with respect to the company
+     * @param burntToACrisp THROW HIM TO THE DOGS!
+     */
+    public static void FireEmployee(Employee burntToACrisp) {
+       Delete(burntToACrisp, EmployeeTable);
+    }
+
+    /**
+     * Need a raise? Need to change address? Look no further!
+     * This employee is already in the table, but its attribute have been changed.
+     * @param worker Employee that has been modified
+     */
+    public static void UpdateEmployee(Employee worker) {
+        FireEmployee(worker);
+        AddEmployee(worker);
+    }
+
+    public static ArrayList<Inventory> AllInventory() {
+        return LookupInventory("","");
+    }
+    public static ArrayList<Inventory> LookupInventory(int attr, String field) {
+        return LookupInventory(Integer.toString(attr), field);
+    }
+    public static ArrayList<Inventory> LookupInventory(String attribute, String field) {
+       return Get(attribute, field, InventoryTable, new Inventory());
+    }
+
+    public static void AddInventory(Inventory product) {
+        Add(product.toString(), InventoryTable);
+    }
+
+    public static void RemoveInventory(Inventory item) {
+        Delete(item, InventoryTable);
+    }
+
+    public static void UpdateInventory(Inventory item) {
+        RemoveInventory(item);
+        AddInventory(item);
+    }
+
+    private static void Add(String values, String table) {
         Connection c = null;
         Statement stmt = null;
         try {
@@ -132,7 +132,7 @@ public class DatabaseManager {
             c.setAutoCommit(false);
             try {
                 stmt = c.createStatement();
-                String sql = "INSERT INTO " + EmployeeTable +
+                String sql = "INSERT INTO " + table +
                         " VALUES (" + values + ");";
                 stmt.executeUpdate(sql);
                 stmt.close();
@@ -148,12 +148,14 @@ public class DatabaseManager {
         }
     }
 
-    /**
-     * Makes the employee die with respect to the company
-     * @param burntToACrisp THROW HIM TO THE DOGS!
-     */
-    public static void FireEmployee(Employee burntToACrisp) {
-        String ID = Integer.toString(burntToACrisp.getId());
+    private static <T> ArrayList<T> Get(String attribute, String field, String table, T obj) {
+        // check for empty args
+        if (attribute.equals("") && !field.equals("")) return null;
+        if (!attribute.equals("") && field.equals("")) return null;
+        // if both are empty, then select everything
+        String selector = (field.equals("") && attribute.equals("")) ?
+                "" : " WHERE " + field + "='" + attribute + "'";
+        ArrayList<T> tableItems = new ArrayList<T>();
         Connection c = null;
         Statement stmt = null;
         try {
@@ -162,7 +164,64 @@ public class DatabaseManager {
             c.setAutoCommit(false);
             try {
                 stmt = c.createStatement();
-                String sql = "DELETE FROM " + EmployeeTable +
+                String sql = "SELECT * FROM " + table + selector;
+                ResultSet rs = stmt.executeQuery(sql);
+
+                if (obj instanceof Employee) {
+                    while (rs.next()) {
+                        ((Employee) obj).setId(rs.getInt("id"));
+                        ((Employee) obj).setFirstName(rs.getString("first_name"));
+                        ((Employee) obj).setLastName(rs.getString("last_name"));
+                        ((Employee) obj).setAddress(rs.getString("address"));
+                        ((Employee) obj).setCity(rs.getString("city"));
+                        ((Employee) obj).setState(rs.getString("state"));
+                        ((Employee) obj).setPhone(rs.getString("phone"));
+                        ((Employee) obj).setEmail(rs.getString("email"));
+                        ((Employee) obj).setSalary(rs.getInt("salary"));
+                        ((Employee) obj).setManager((rs.getString("manager")).equals("true"));
+                        tableItems.add(obj);
+                    }
+                }
+                else if (obj instanceof Inventory) {
+                    while (rs.next()) {
+                        ((Inventory) obj).setId(rs.getInt("id"));
+                        ((Inventory) obj).setItem(rs.getString("item"));
+                        ((Inventory) obj).setStock(rs.getInt("stock"));
+                        tableItems.add(obj);
+                    }
+                }
+                rs.close();
+                stmt.close();
+            } catch (SQLException E) {
+                E.printStackTrace();
+            }
+            c.close();
+        } catch (SQLException E) {
+            E.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return tableItems;
+    }
+
+    private static <T> void Delete(T obj, String table) {
+        String ID = "";
+        if (obj instanceof Employee) {
+            ID = Integer.toString(((Employee) obj).getId());
+        }
+        else if (obj instanceof Inventory) {
+            ID = Integer.toString(((Inventory) obj).getId());
+        }
+        if (ID.isEmpty()) return;
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName(DatabaseDriver);
+            c = DriverManager.getConnection(SalonDatabase);
+            c.setAutoCommit(false);
+            try {
+                stmt = c.createStatement();
+                String sql = "DELETE FROM " + table +
                         " WHERE ID=" + ID;
                 stmt.executeUpdate(sql);
                 stmt.close();
@@ -176,15 +235,5 @@ public class DatabaseManager {
         } catch (SQLException E) {
             E.printStackTrace();
         }
-    }
-
-    /**
-     * Need a raise? Need to change address? Look no further!
-     * This employee is already in the table, but its attribute have been changed.
-     * @param worker Employee that has been modified
-     */
-    public static void UpdateEmployee(Employee worker) {
-        FireEmployee(worker);
-        AddEmployee(worker);
     }
 }
---
+++ src/Inventory.java
@@ -0,0 +1,47 @@
+/**
+ * @author Matthew Meyer
+ */
+public class Inventory {
+
+    private int id;
+    private String item;
+    private int stock;
+
+    Inventory() {}
+    Inventory(int _id, String _item, int _stock) {
+        id = _id;
+        item = _item;
+        stock = _stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getItem() {
+        return item;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setItem(String item) {
+        this.item = item;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(id) + ",'" +
+                item + "', " + stock;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Inventory && toString().equals(obj.toString()));
+    }
+
+}
