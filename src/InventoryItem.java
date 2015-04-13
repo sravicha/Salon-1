0d4e5d52942acb131545aea2aca4c302d2f56ffd
Lejxk <gday4@slu.edu>
2015-04-13 13:13:04 -0500

    Create InventoryItem.java

---
+++ src/InventoryItem.java
@@ -0,0 +1,46 @@
+/** a
+* @author Graison Day
+*/
+// A class meant for items that are supposed to go into inventories
+
+public class InventoryItem {
+    
+    private String name;
+    private int id;
+    private int stock;
+    
+    InventoryItem() {}
+    InventoryItem(String aName, int anId, int aStock){
+      name = aName;
+      id = anId;
+      stock = aStock;
+    )
+      
+    public String getName() {
+      return name;
+    }
+    
+    public int getId() {
+      return id;
+      )
+      
+    public int getStock() {
+      return stock;
+    }
+    
+    public String setName(String newName) {
+      name = newName;
+    }
+    
+    public int setId(int newId) {
+      id = newId;
+    }
+    
+    public int setStock(int newStock) {
+      stock = newStock;
+    }
+    
+    public int addToStock(int addition) {
+      stock += addition;
+      }
+     
